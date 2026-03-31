b5d5d8ed6e864c35f77c84cb008816cf7a0ee3c2
Bruno Vieira <brunolcvieira3@gmail.com>
2026-03-31 10:07:15 -0300

    add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,32 @@
+import Script from "next/script";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18051800466"
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18051800466');
+          `}
+        </Script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
+
+
 import type { Metadata } from "next";
 import "./globals.css";
 
