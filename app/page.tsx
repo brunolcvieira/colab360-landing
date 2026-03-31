f9cb15808079c932456ebfa16253097e53883c91
brunolcvieira <brunolcvieira3@gmail.com>
2026-03-31 18:54:34 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,5 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import ProofBar from "@/components/ProofBar";
-import StatsBar from "@/components/StatsBar";
-import Problema from "@/components/Problema";
-import Solucao from "@/components/Solucao";
-import Beneficios from "@/components/Beneficios";
-import ComoFunciona from "@/components/ComoFunciona";
-import Diferenciais from "@/components/Diferenciais";
-import CtaFinal from "@/components/CtaFinal";
-import Footer from "@/components/Footer";
-import WhatsappFloat from "@/components/WhatsappFloat";
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <ProofBar />
-      <StatsBar />
-      <Problema />
-      <Solucao />
-      <Beneficios />
-      <ComoFunciona />
-      <Diferenciais />
-      <CtaFinal />
-      <Footer />
-      <WhatsappFloat />
-    </>
-  );
+  redirect('/landing.html')
 }
